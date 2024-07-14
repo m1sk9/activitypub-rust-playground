8311e5ceb82b204acabcdb77b0c8965181d96d4e
m1sk9 <me@m1sk9.dev>
2024-07-15 02:39:48 +0900

    feat: add response actor

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@ use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 #[get("/")]
-async fn index(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-        .content_type("text/plain")
-        .body("Hello world!")
+async fn parson(req: HttpRequest) -> impl Responder {
+    match req.headers().get("Accept") {
+        Some(a) if a == "application/activity+json" => {
+            let file = std::fs::read_to_string("./resources/actor.json").unwrap();
+            HttpResponse::Ok()
+                .content_type("application/activity+json")
+                .body(file)
+        }
+        _ => HttpResponse::NotAcceptable().finish(),
+    }
 }
 
 #[actix_web::main]
@@ -18,7 +24,7 @@ async fn main() -> std::io::Result<()> {
         .set_certificate_chain_file("./resources/localhost+1.pem")
         .unwrap();
 
-    HttpServer::new(|| App::new().service(index))
+    HttpServer::new(|| App::new().service(parson))
         .bind_openssl("127.0.0.1:8080", builder)?
         .run()
         .await
