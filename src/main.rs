b9b8bce22895c9195a91fe00084b17daaa404e5e
m1sk9 <me@m1sk9.dev>
2024-07-15 00:51:38 +0900

    setup actix_web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn index(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("text/plain")
+        .body("Hello world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(index))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
