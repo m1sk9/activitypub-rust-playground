84f90e2118411422ca323582cb29828a8a5644b5
m1sk9 <me@m1sk9.dev>
2024-07-15 01:11:39 +0900

    setup https request

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 
 #[get("/")]
 async fn index(_req: HttpRequest) -> impl Responder {
@@ -9,8 +10,16 @@ async fn index(_req: HttpRequest) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let mut builder = SslAcceptor::mozilla_modern_v5(SslMethod::tls_server()).unwrap();
+    builder
+        .set_private_key_file("./resources/localhost+1-key.pem", SslFiletype::PEM)
+        .unwrap();
+    builder
+        .set_certificate_chain_file("./resources/localhost+1.pem")
+        .unwrap();
+
     HttpServer::new(|| App::new().service(index))
-        .bind(("127.0.0.1", 8080))?
+        .bind_openssl("127.0.0.1:8080", builder)?
         .run()
         .await
 }
